4b5e739a7425207a4d2b8d46b0414e5fcd9682a6
Rui Neves <49654391+ruisoho@users.noreply.github.com>
2025-08-29 11:52:25 +0200

    Update next.config.js to use new serverExternalPackages option

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    serverComponentsExternalPackages: ['@prisma/client', 'prisma']
-  },
+  serverExternalPackages: ['@prisma/client', 'prisma'],
   images: {
     domains: ['images.unsplash.com', 'unsplash.com']
   }
